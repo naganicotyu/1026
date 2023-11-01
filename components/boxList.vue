89f7771567d397d81aa50f9ac7be0b6d477448a9
naganicotyu <niconico.guruhirorin.324@gmail.com>
2023-11-01 17:28:58 +0900

    boxlist fixed

--- components/boxList.vue
+++ components/boxList.vue
@@ -1,111 +1,111 @@
 <template>
 <div class="boxList">
     <div  v-for="box in boxes" :key="box.title" class="boxListVfor">
-        <div v-bind:class='[boxCommon,box.boxNumber,{active:isActive}]'>
 
-            <p class="boxTitle">
-                {{box.title}}
-            </p>
-
-            <p class="boxText">
-                {{box.primary}}
-            </p>
-
-        </div>
-
+            <details v-bind:class="[box.boxNumber]">
+                <summary class="boxTitle">{{box.title}}</summary>
+                <p class="boxText">{{box.primary}}</p>
+            </details>
         </div>
     </div>
 </template>
 
 <script>
-    export default{auth:false,data(){return{boxes:[
-                {boxNumber:"box1",title:"プロフィール",primary:"わたしについて。"},
-                {boxNumber:"box2",title:"twitter",primary:"twitterです。"},
-                {boxNumber:"box3",title:"絵",primary:"これは絵です"},
-                {boxNumber:"box4",title:"スケジュール",primary:"これからのスケジュールｼﾞｭﾙ"},
-    ]}}}
+    export default{
+        auth:false,
+        
+            data(){
+            return{
+            isActive:true,
+            boxes:[
+                {boxNumber:"box1",title:"プロフィール",primary:"わたしについて。display:blockの要素に対しては、margin-leftとmargin-rightのそれぞれの値をautoにすることで水平方向に中央寄せできます。",isActive:false},
+                {boxNumber:"box2",title:"twitter",primary:"twitterです。",isActive:false},
+                {boxNumber:"box3",title:"絵",primary:"これは絵です",isActive:false},
+                {boxNumber:"box4",title:"スケジュール",primary:"これからのスケジュールｼﾞｭﾙ",isActive:false},
+                ]
+            }
+        },
+    
+    //methods:{
+    //toggle(){
+      //  this.isActive=!this.isActive
+   // }
+    //}
+
+
+}
+
 
 </script>
 
 <style>
 .boxList{
     margin:10px;
-    width: 800px;
+    width: 600px;
     height:600px;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 2fr 1fr;
+    margin-top: 0px;
     margin-left: auto;
     margin-right: auto;
+    transition: transform 0.4s;
+
 }
 
+
+
 .box1,.box2,.box3,.box4{
+    position:relative;
     border:solid #00DC82 1px;
+    height:auto;
+    margin: 4px;
+    transition: transform 0.4s;
 }
 
  .box1{
-    grid-row:2/3;
-    grid-column:1/2;
 }
 
 
 .box2{
     grid-row:2/3;
-    grid-column:2/3;
 }
 
 .box3{
-    grid-row:4/3;
-    grid-column:1/2;
+    grid-row:3/4;
 }
-
 .box4{
-    grid-row:3/4;
-    grid-column:2/3;
-}
 
-.box1 active{
-    grid-row:2/3;
-    grid-column:1/3;
+
 }
 
 
-.box2 active{
-    grid-row:2/3;
-    grid-column:1/3;
+.boxTitle{
+margin-top: auto;
+padding:10px;
 }
 
-.box3 active{
-    grid-row:4/3;
-    grid-column:1/3;
+summary{position:relative;display: block;padding-left: 12px;}
+summary::-webkit-details-marker{desplay:none;}
+
+summary:before,summary:after{
+    content:"";
+    margin:auto;
+    position:absolute;
+    top:0;
+    bottom:0;
+    left:0;
+}
+summary::before{
+    width:12px;
+    height:12px;
+    background-color: rgb(44, 157, 255);
 }
 
-.box4 active{
-    grid-row:3/4;
-    grid-column:1/3;
-}
-
-.box1 up{
-    grid-row:1/2;
-    grid-column:1/2;
+summary::after{
+    width:12px;
+    height:12px;
+    background-color: aliceblue;
 }
 
 
-.box2 up{
-    grid-row:1/2;
-    grid-column:2/3;
-}
-
-.box3 up{
-    grid-row:4/5;
-    grid-column:1/2;
-}
-
-.box4 up{
-    grid-row:4/5;
-    grid-column:2/3;
-}
-.boxTitle{}
 
 .boxText{}
 </style>
