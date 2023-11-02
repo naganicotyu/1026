a75cea0d3345241f0ca6d9002fdb0e8ea4979748
naganicotyu <niconico.guruhirorin.324@gmail.com>
2023-11-02 16:55:47 +0900

    what is [active]?

--- components/boxList.vue
+++ components/boxList.vue
@@ -2,22 +2,23 @@
 <div class="boxList">
     <div  v-for="box in boxes" :key="box.title" class="boxListVfor">
 
-            <details v-bind:class="[box.boxNumber]">
-                <summary class="boxTitle">{{box.title}}</summary>
-                <p class="boxText">{{box.primary}}</p>
-            </details>
-        </div>
+            <div v-bind:class="box.boxNumber">
+                <div @click="onClickTitle()" class="boxTitle">{{box.title}}</div>
+                <transition name="boxTextSwitch">
+                <div v-show="active" class="boxText"><p>{{box.primary}}</p></div>
+            </transition>
+            </div>
+            </div>
     </div>
 </template>
 
 <script>
     export default{
-        auth:false,
         
             data(){
             return{
-            isActive:true,
-            boxes:[
+                active:false,
+                boxes:[
                 {boxNumber:"box1",title:"プロフィール",primary:"わたしについて。display:blockの要素に対しては、margin-leftとmargin-rightのそれぞれの値をautoにすることで水平方向に中央寄せできます。",isActive:false},
                 {boxNumber:"box2",title:"twitter",primary:"twitterです。",isActive:false},
                 {boxNumber:"box3",title:"絵",primary:"これは絵です",isActive:false},
@@ -26,12 +27,11 @@
             }
         },
     
-    //methods:{
-    //toggle(){
-      //  this.isActive=!this.isActive
-   // }
-    //}
-
+    methods:{
+    onClickTitle(){
+            this.active=!this.active;
+        }
+    }
 
 }
 
@@ -49,27 +49,45 @@
     transition: transform 0.4s;
 
 }
+.boxTitle{
+    margin-top: auto;
+    margin-right: 0px;
+    padding:10px;
+    border:solid #00DC82 1px;
+}
+
+.boxPerents{
+    position:relative;display: block;
+    padding-left: 0px;}
+
+.boxText{
+    height:200px;
+    padding:10px;
+    overflow: hidden;
+}
+
+.boxTextSwitch-leave-active,.boxTextSwitch-enter-active{
+    transition:height 0.5s linear;
+}
+.boxTextSwitch-enter,.boxTextSwitch-leave-to{
+    height:0;
+}
 
 
 
 .box1,.box2,.box3,.box4{
     position:relative;
-    border:solid #00DC82 1px;
+    border:solid #ef11f7 3px;
     height:auto;
     margin: 4px;
     transition: transform 0.4s;
 }
 
  .box1{
-}
-
-
-.box2{
-    grid-row:2/3;
-}
+    }
 
 .box3{
-    grid-row:3/4;
+    
 }
 .box4{
 
@@ -77,35 +95,5 @@
 }
 
 
-.boxTitle{
-margin-top: auto;
-padding:10px;
-}
 
-summary{position:relative;display: block;padding-left: 12px;}
-summary::-webkit-details-marker{desplay:none;}
-
-summary:before,summary:after{
-    content:"";
-    margin:auto;
-    position:absolute;
-    top:0;
-    bottom:0;
-    left:0;
-}
-summary::before{
-    width:12px;
-    height:12px;
-    background-color: rgb(44, 157, 255);
-}
-
-summary::after{
-    width:12px;
-    height:12px;
-    background-color: aliceblue;
-}
-
-
-
-.boxText{}
 </style>
